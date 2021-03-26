ad9efd8fa8ed69c817cc2da478500d7a421e4ea7
WesCat <wesleydeckard@hotmail.com>
2021-03-26 18:55:28 -0500

    added changes for unit tests and all structure

---
+++ twitter-app-tests/ReportingDataTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace twitter_app_tests
+{
+    [TestClass]
+    public class ReportingDataTests
+    {
+      //  private TwitterResponse
+
+        [TestInitialize]
+        public void Init() 
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+}
