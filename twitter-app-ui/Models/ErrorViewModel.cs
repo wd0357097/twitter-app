01ac9d5e4a44b22f1f609a5aa2b60345ea7533ce
WesCat <wesleydeckard@hotmail.com>
2021-03-28 18:35:25 -0500

    added new project for web front end

---
+++ twitter-app-ui/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace twitter_app_ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
