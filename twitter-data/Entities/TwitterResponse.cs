a834d6ef803a362d4a20ba65ad61aed77f1c5374
WesCat <wesleydeckard@hotmail.com>
2021-03-30 08:53:10 -0500

    updated twitter data object

--- twitter-data/Entities/TwitterResponse.cs
+++ twitter-data/Entities/TwitterResponse.cs
@@ -1,5 +1,6 @@
 ﻿
 using Newtonsoft.Json;
+using System;
 
 namespace twitter_data.Entities
 { 
@@ -7,6 +8,9 @@ namespace twitter_data.Entities
     {
         [JsonProperty("data")]
         public Data Data { get; set; }
+
+        [JsonProperty("includes")]
+        public Includes Includes { get; set; }
     }
 
     public class Data
@@ -16,5 +20,95 @@ namespace twitter_data.Entities
 
         [JsonProperty("text")]
         public string Text { get; set; }
+
+        [JsonProperty("entities")]
+        public Entities Entities { get; set; }
+
+        [JsonProperty("attachments")]
+        public Attachments Attachments { get; set; }
+    }
+
+    public partial class Attachments
+    {
+        [JsonProperty("media_keys")]
+        public string[] MediaKeys { get; set; }
+    }
+
+    public partial class Includes
+    {
+        [JsonProperty("media")]
+        public Media[] Media { get; set; }
+    }
+
+    public partial class Media
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("media_key")]
+        public string MediaKey { get; set; }
+
+        [JsonProperty("preview_image_url")]
+        public Uri PreviewImageUrl { get; set; }
+    }
+
+    public partial class Entities
+    {
+        [JsonProperty("hashtags")]
+        public HashTag[] Hashtags { get; set; }
+
+        [JsonProperty("urls")]
+        public Url[] Urls { get; set; }
+    }
+
+    public partial class Annotation
+    {
+        [JsonProperty("start")]
+        public long Start { get; set; }
+
+        [JsonProperty("end")]
+        public long End { get; set; }
+
+        [JsonProperty("probability")]
+        public double Probability { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("normalized_text")]
+        public string NormalizedText { get; set; }
+    }
+
+    public partial class HashTag
+    {
+        [JsonProperty("start")]
+        public long Start { get; set; }
+
+        [JsonProperty("end")]
+        public long End { get; set; }
+
+        [JsonProperty("tag")]
+        public string Tag { get; set; }
+    }
+
+    public partial class Url
+    {
+        [JsonProperty("start")]
+        public long Start { get; set; }
+
+        [JsonProperty("end")]
+        public long End { get; set; }
+
+        [JsonProperty("url")]
+        public Uri UrlUrl { get; set; }
+
+        [JsonProperty("expanded_url")]
+        public Uri ExpandedUrl { get; set; }
+
+        [JsonProperty("display_url")]
+        public string DisplayUrl { get; set; }
     }
 }
+
+
+
