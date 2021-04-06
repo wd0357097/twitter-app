552b8bd77ed7b89177d2df40d2b5f811e0d504e8
WesCat <wesleydeckard@hotmail.com>
2021-04-06 18:50:19 -0500

    added DI to the console app

--- twitter-app-console/Program.cs
+++ twitter-app-console/Program.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using twitter_data.Interface;
 using twitter_data.Managers;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace twitter_app_console
 {
@@ -13,10 +14,17 @@ namespace twitter_app_console
     {
         static async Task Main(string[] args)
         {
+            //setup our DI
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IAppStream>(x =>
+                    new TwitterStream(ConfigurationManager.AppSettings["token"]))
+                .BuildServiceProvider();
+            // get service from DI
+            var data = serviceProvider.GetService<IAppStream>();
+
             // Event handlers to report data 
             // https://stackoverflow.com/questions/44366270/implementing-a-custom-event-handler-from-interface
             Console.OutputEncoding = Encoding.UTF8;
-            IAppStream data = new TwitterStream(ConfigurationManager.AppSettings["token"]);   
             Console.WriteLine("Welcome to the Twitter Sample Stream Data Collection");
             Console.WriteLine("Press any key to start the twitter stream...");
             Console.ReadKey();// wait for the 'any key' to be pressed, 'where's the any key??'
